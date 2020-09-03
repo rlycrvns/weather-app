57c0308b8935493f0dcb8a91d2b8fbd2e0257ab4
rlycrvns <rileycravens@Rileys-MacBook-Pro.local>
2020-09-03 15:25:26 -0700

    add: init/call ui paint

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Init Weath object
 const weather = new Weather('Portland', 'OR', 'USA');
+// Init UI
+const ui = new UI();
 
 // Get Weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
@@ -8,7 +10,7 @@ function getWeather() {
   weather
     .getWeather()
     .then((results) => {
-      console.log(results);
+      ui.paint(results);
     })
     .catch((err) => console.log(err));
 }
